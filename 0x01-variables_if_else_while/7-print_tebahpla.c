07c9ede3a25e835b7b7c854886df95a648f0d199
Wise4lyf <wisdoms85@yahoo.com>
2022-10-15 00:08:35 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,10 +4,10 @@
  *Return:0 if no errors, non zero if errors
  */
 int main(void)
-{
- char i;
- for (i = 'z'; i >= 'a'; i--)
- putchar(i);
- putchar('\n');
- return (0);
-}
+ {
+  char i;
+  for (i = 'z'; i >= 'a'; i--)
+  putchar(i);
+  putchar('\n');
+  return (0);
+ }
